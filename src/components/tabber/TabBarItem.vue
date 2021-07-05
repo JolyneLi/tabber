b7965f69e124fae594d7278d002c02340e704cfc
cyl <shinian7778@163.com>
2021-07-05 17:12:27 +0800

    first commit

---
+++ src/components/tabber/TabBarItem.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="tab-bar-item">
+    <div :class="{ active: isactive }">
+      <slot name="icon"></slot>
+      <slot name="text"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TabBarItem",
+  data() {
+    return {
+      isactive: true,
+    };
+  },
+};
+</script>
+
+<style scoped>
+.tab-bar-item {
+  flex: 1;
+  text-align: center;
+  height: 49px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+.active {
+  color: red;
+}
+</style>
